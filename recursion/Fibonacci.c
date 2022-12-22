88c61fd24f930b954d06a508f4a4e69523ede566
Vividh Pandey <vividh.pandey.13@gmail.com>
2022-12-22 16:04:07 +0530

    Added: Fibonacci.c

---
+++ recursion/Fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+// Basic-Generalized Approach for finding fibonacci
+int fib(int n)
+{
+    if (n <= 1)
+        return n;
+    return fib(n - 1) + fib(n - 2);
+}
+int F[10] = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
+
+// Optimized Approach that uses the concept of Memoization for finding Fibonacci
+int fibopt(int n)
+{
+    if (n <= 1)
+    {
+        F[n] = n;
+        return n;
+    }
+    else
+    {
+        if (F[n - 2] == -1)
+            F[n - 2] = fibopt(n - 2);
+        if (F[n - 1] == -1)
+            F[n - 1] = fibopt(n - 1);
+        return F[n - 1] + F[n - 2];
+    }
+}
+int main()
+{
+    printf("%d  \n", fib(6));
+    printf("%d  \n", fibopt(6));
+    return 0;
+}
