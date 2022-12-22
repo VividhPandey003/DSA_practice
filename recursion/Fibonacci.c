d6d9489c5c8e8417aed103833429e291574f137d
Vividh Pandey <vividh.pandey.13@gmail.com>
2022-12-22 16:05:39 +0530

    Update Fibonacci.c

--- recursion/Fibonacci.c
+++ recursion/Fibonacci.c
@@ -7,7 +7,7 @@ int fib(int n)
         return n;
     return fib(n - 1) + fib(n - 2);
 }
-int F[10] = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
+int F[10];
 
 // Optimized Approach that uses the concept of Memoization for finding Fibonacci
 int fibopt(int n)
@@ -28,6 +28,9 @@ int fibopt(int n)
 }
 int main()
 {
+    int i = 0;
+    for (i = 0; i < 10; i++)
+        F[i] = -1;
     printf("%d  \n", fib(6));
     printf("%d  \n", fibopt(6));
     return 0;
